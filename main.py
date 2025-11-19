16c7b0ad2dc4ded9d9237bdc4f369967490201bb
JulioCaue <juliocaue22@gmail.com>
2025-11-19 16:21:57 -0300

    Nova versão da lista de tarefas, contem bugs.

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 Lista_de_tarefas=[]
 while True:
-    escolha=input('     Menu da Lista de tarefas\n\n1. Adicionar Tarefa\n2. Listar Tarefas\n3. Sair ')
+    escolha=input('     Menu da Lista de tarefas\n\n1. Adicionar Tarefa\n2. Listar Tarefas\n3. deletar tarefas\n4. Sair\n\nEscolha um numero: ')
     try:
         int(escolha)
     except ValueError:
         print ('Opção Invalida.')
     else:
-        if int(escolha)>0 and int(escolha)<=3:
-            if str(escolha)==('1'):
-                Lista_de_tarefas.append (input("Adicione a tarefa: "))
+        if int(escolha)>0 and int(escolha)<=4:
+            if str(escolha)==('1') and Lista_de_tarefas==[]:
+                Lista_de_tarefas.insert(1,input("Adicione a tarefa: "))
+            if str(escolha)==('1') and Lista_de_tarefas!=[]:
+                Lista_de_tarefas.append(input("Adicione a tarefa: "))
             elif str(escolha)==('2'):
                 if Lista_de_tarefas==[]:
                     print ('Nenhuma tarefa.')
                 else:
                     print ('Lista de tarefas:')
-                    for tarefa in Lista_de_tarefas:
+                    for tarefa in enumerate(Lista_de_tarefas,1):
                         print (tarefa)
+            elif str(escolha)=='3':
+                while True:
+                    item_a_ser_deletado= input('Escreva o numero da tarefa a deletar ou digite sair para cancelar: ')
+                    try:
+                        str(item_a_ser_deletado)
+                    except:
+                        print ('Um erro aconteceu.')
+                    else:
+                        if str(item_a_ser_deletado)==str('sair'):
+                            break
+                    try:
+                        int(item_a_ser_deletado)
+                    except:
+                        print("tarefa não existe.")
+                  
+                    if int(item_a_ser_deletado)<len(Lista_de_tarefas):
+                        Lista_de_tarefas.pop(item_a_ser_deletado)
+                        print ("Tarefa foi deletada.")
+                        break
+                    else:
+                        print('item não faz parte da lista.')
             else:
                 break
         else:
